db92dac3e12ab82edb2151064474eb1d1afbb381
Alastair Bull <alastair.bull@yahoo.com>
2018-04-10 15:08:41 -0400

    attraction setup

---
+++ Capstone.Web/Controllers/AttractionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Globalization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Capstone.Web.Authentication;
+using Capstone.Web.Models;
+
+
+namespace Capstone.Web.Controllers
+{
+    public class AttractionController : Controller
+    {
+        // GET: AttractionDetail
+        public ActionResult Attraction()
+        {
+            return View();
+        }
+    }
+}
