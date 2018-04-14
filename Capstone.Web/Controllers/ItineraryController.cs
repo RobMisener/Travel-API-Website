aabef383708a58783e20bf33e9ce036128b28207
Alastair Bull <alastair.bull@yahoo.com>
2018-04-14 11:34:13 -0400

    controller pt 2

--- Capstone.Web/Controllers/ItineraryController.cs
+++ Capstone.Web/Controllers/ItineraryController.cs
@@ -1,5 +1,4 @@
-﻿using Capstone.Web.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,6 +6,7 @@ using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Http;
+using Capstone.Web.Models;
 using Capstone.Web.DAL;
 
 namespace Capstone.Web.Controllers
@@ -21,23 +21,44 @@ namespace Capstone.Web.Controllers
 			this.dal = dal;
 		}
 
-        // GET: Survey
-        public ActionResult Itinerary()
+        // GET: Itinerary
+        public ActionResult GetItinerary()
         {
             List<ItineraryModel> itineraryModelList = dal.GetItinerary();
 
-            return View(itineraryModelList);
+            return Ok(itineraryModelList);
         }
 
+        //POST: Create Itinerary
+        [HttpPost]
         [Route("api/itinerary/")]
-		[HttpPost]
-		public IHttpActionResult Itinerary(ItineraryModel model)
+		public IHttpActionResult CreateItinerary(ItineraryModel model)
 		{
 
             bool result = dal.CreateItinerary(model);
             return RedirectToAction("Itinerary");
 
 		}
+        //POST: Update Itinerary
+        [HttpPost]
+        [Route("api/itinerary/")]
+        public IHttpActionResult UpdateItinerary(ItineraryModel model)
+        {
+
+            bool result = dal.UpdateItinerary(model);
+            return RedirectToAction("Itinerary");
+
+        }
+        //DELETE: Delete Itinerary
+        [HttpDelete]
+        [Route("api/itinerary/")]
+        public IHttpActionResult DeleteItinerary(ItineraryModel model)
+        {
+
+            bool result = dal.DeleteItinerary(model);
+            return RedirectToAction("Itinerary");
+
+        }
 
 
     }
