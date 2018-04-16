60e104a817d43ca1d34724ecaa8b58d7e855ee7a
Robert Misener <Misener05@gmail.com>
2018-04-16 02:00:36 -0400

    temporarly commented out the previous to prevent conflicts in running

--- Capstone.Web/Models/RatedPlacesModel.cs
+++ Capstone.Web/Models/RatedPlacesModel.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
 
 
-/// <summary>
-/// Model class to store the places (stores thumbs up/down info for them)
-/// </summary>
+///// <summary>
+///// Model class to store the places (stores thumbs up/down info for them)
+///// </summary>
 
-namespace Capstone.Web.Models
-{
-	public class RatedPlacesModel
-	{
-		public string PlaceId { get; set; }
-		public string PlaceName { get; set; }
-		public int ThumbsUpCount { get; set; }
-		public int ThumbsDownCount { get; set; }
-	}
-}
+//namespace Capstone.Web.Models
+//{
+//	public class RatedPlacesModel
+//	{
+//		public string PlaceId { get; set; }
+//		public string PlaceName { get; set; }
+//		public int ThumbsUpCount { get; set; }
+//		public int ThumbsDownCount { get; set; }
+//	}
+//}
