39f6441b94e05fa1cfda55c7ee5b869c359ff902
Rob Misener <misener05@gmail.com>
2018-04-16 09:51:06 -0400

    dal update

--- Capstone.Web/DAL/RatedPlacesDALcs.cs
+++ Capstone.Web/DAL/RatedPlacesDALcs.cs
@@ -1,49 +1,47 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
-//namespace Capstone.Web.DAL
-//{
-//	public class RatedPlacesDALcs
-//	{
+namespace Capstone.Web.DAL
+{
 
-//		public bool AddPlaceToDatabase(string placeId)
-//		{
-//				try
-//				{
-//					using (TransactionScope scope = new TransactionScope())
-//					{
-//						using (SqlConnection conn = new SqlConnection(connectionString))
-//						{
-//							conn.Open();
-//							SqlCommand cmd = new SqlCommand("INSERT INTO Itinerary (placeId) VALUES (@PlaceId)", conn);
-//							cmd.Parameters.AddWithValue("@PlaceId", placeId);
+	public class RatedPlacesDALcs
+	{
+		public bool AddPlaceToDatabase(string placeId)
+		{
+			try
+			{
+				using (TransactionScope scope = new TransactionScope())
+				{
+					using (SqlConnection conn = new SqlConnection(connectionString))
+					{
+						conn.Open();
+						SqlCommand cmd = new SqlCommand("INSERT INTO [Rated_Places] (placeId) VALUES (@PlaceId)", conn);
+						cmd.Parameters.AddWithValue("@PlaceId", placeId);
+						cmd.ExecuteNonQuery();
 
-//							cmd.ExecuteNonQuery();
+						//cmd = new SqlCommand(@"Select * from Rated_Places", conn);
+						//SqlDataReader reader = cmd.ExecuteReader();
 
-//							//cmd = new SqlCommand(@"Select * from Rated_Places", conn);
-//							//SqlDataReader reader = cmd.ExecuteReader();
+						//if (reader.Read())
+						//{
 
-//							//if (reader.Read())
-//							//{
+						//		SqlCommand newcmd = new SqlCommand("INSERT INTO Itinerary (placeId) VALUES (@PlaceId), conn);
+						//		cmd.Parameters.AddWithValue("@PlaceId", placeId);
 
-//							//		SqlCommand newcmd = new SqlCommand("INSERT INTO Itinerary (placeId) VALUES (@PlaceId), conn);
-//							//		cmd.Parameters.AddWithValue("@PlaceId", placeId);
-
-//							//		cmd.ExecuteNonQuery();
-//							//}
-//						}
-//						scope.Complete();
-//					}
-//					return true;
-//				}
-//				catch (SqlException ex)
-//				{
-//					Console.WriteLine("An error occurred reading the database: " + ex.Message);
-//					return false;
-//				}
-//			}
-
-//		}
-//}
+						//		cmd.ExecuteNonQuery();
+						//}
+					}
+					scope.Complete();
+				}
+				return true;
+			}
+			catch (SqlException ex)
+			{
+				Console.WriteLine("An error occurred reading the database: " + ex.Message);
+				return false;
+			}
+		}
+	}
+}
