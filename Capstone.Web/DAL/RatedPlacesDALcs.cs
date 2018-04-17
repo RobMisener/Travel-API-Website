c103c13e496c1a3b9731536bd7842ce8166f09a8
Rob Misener <misener05@gmail.com>
2018-04-17 14:07:59 -0400

    added methods to RatedPlacesModel

--- Capstone.Web/DAL/RatedPlacesDALcs.cs
+++ Capstone.Web/DAL/RatedPlacesDALcs.cs
@@ -1,47 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
 
-namespace Capstone.Web.DAL
-{
+//namespace Capstone.Web.DAL
+//{
+//	public class RatedPlacesDALcs
+//	{
+//		public ItineraryDAL(string connectionString)
+//		{
+//			this.connectionString = connectionString;
+//		}
 
-	public class RatedPlacesDALcs
-	{
-		//public bool AddPlaceToDatabase(string placeId)
-		//{
-		//	try
-		//	{
-		//		using (TransactionScope scope = new TransactionScope())
-		//		{
-		//			using (SqlConnection conn = new SqlConnection(connectionString))
-		//			{
-		//				conn.Open();
-		//				SqlCommand cmd = new SqlCommand("INSERT INTO [Rated_Places] (placeId) VALUES (@PlaceId)", conn);
-		//				cmd.Parameters.AddWithValue("@PlaceId", placeId);
-		//				cmd.ExecuteNonQuery();
+//		private const string SQL_GetItinerary = @"Select *  from Rated_Places";
 
-		//				//cmd = new SqlCommand(@"Select * from Rated_Places", conn);
-		//				//SqlDataReader reader = cmd.ExecuteReader();
+//		string connectionString;
 
-		//				//if (reader.Read())
-		//				//{
+//		public bool AddPlaceToDatabase(string placeId)
+//		{
+//			try
+//			{
+//				using (TransactionScope scope = new TransactionScope())
+//				{
+//					using (SqlConnection conn = new SqlConnection(connectionString))
+//					{
+//						conn.Open();
+//						SqlCommand cmd = new SqlCommand("INSERT INTO [Rated_Places] (PlaceId) VALUES (@placeId)", conn);
+//						cmd.Parameters.AddWithValue("@PlaceId", placeId);
+//						cmd.ExecuteNonQuery();
 
-		//				//		SqlCommand newcmd = new SqlCommand("INSERT INTO Itinerary (placeId) VALUES (@PlaceId), conn);
-		//				//		cmd.Parameters.AddWithValue("@PlaceId", placeId);
+//					}
+//					scope.Complete();
+//				}
+//				return true;
+//			}
+//			catch (SqlException ex)
+//			{
+//				Console.WriteLine("An error occurred reading the database: " + ex.Message);
+//				return false;
+//			}
+//		}
 
-		//				//		cmd.ExecuteNonQuery();
-		//				//}
-		//			}
-		//			scope.Complete();
-		//		}
-		//		return true;
-		//	}
-		//	catch (SqlException ex)
-		//	{
-		//		Console.WriteLine("An error occurred reading the database: " + ex.Message);
-		//		return false;
-		//	}
-		//}
-	}
-}
+//		public List<RatedPlacesModel> GetListRatedPlaces()
+//		{
+//			List<RatedPlacesModel> results = new List<RatedPlacesModel>();
+
+//			try
+//			{
+//				using (SqlConnection conn = new SqlConnection(connectionString))
+//				{
+
+//					conn.Open();
+
+//					SqlCommand cmd = new SqlCommand(SQL_SelectSurveyCountByParkCode, conn);
+
+//					SqlDataReader reader = cmd.ExecuteReader();
+
+//					while (reader.Read())
+//					{
+//						RatedPlacesModel result = new RatedPlacesModel
+//						{
+//							PlaceName = Convert.ToInt32(reader["surveyCount"]),
+//							ThumbsUpCount = Convert.ToString(reader["parkCode"]),
+//							ThumbsDownCount = Convert.ToString(reader["parkName"])
+//						};
+
+//						results.Add(result);
+//					}
+
+//					return results;
+//				}
+//			}
+//			catch (SqlException)
+//			{
+//				throw;
+//			}
+//		}
+//	}
+//}
